ae8ae2bf4096531d8309ff63d5292db352a04e6f
qiuxin2012 <qiuxin2012cs@gmail.com>
2026-01-12 13:19:24 +0800

    update

--- cmake/Modules/FindoneDNN.cmake
+++ cmake/Modules/FindoneDNN.cmake
@@ -1,93 +1,84 @@
-# * Try to find oneDNN
+# - Try to find oneDNN
 #
-# The following are set after configuration is done: ONEDNN_FOUND          : set
-# to true if oneDNN is found. ONEDNN_INCLUDE_DIR    : path to oneDNN include
-# dir. ONEDNN_LIBRARY        : list of libraries for oneDNN
+# The following are set after configuration is done:
+#  ONEDNN_FOUND          : set to true if oneDNN is found.
+#  ONEDNN_INCLUDE_DIR    : path to oneDNN include dir.
+#  ONEDNN_LIBRARY        : list of libraries for oneDNN
 #
 
-if(NOT ONEDNN_FOUND)
-  set(ONEDNN_FOUND OFF)
+IF (NOT ONEDNN_FOUND)
+SET(ONEDNN_FOUND OFF)
 
-  set(ONEDNN_LIBRARY)
-  set(ONEDNN_INCLUDE_DIR)
-  set(DNNL_INCLUDES)
+SET(ONEDNN_LIBRARY)
+SET(ONEDNN_INCLUDE_DIR)
+SET(DNNL_INCLUDES)
 
-  set(THIRD_PARTY_DIR "${PROJECT_SOURCE_DIR}/third_party")
-  set(ONEDNN_DIR "oneDNN")
-  set(ONEDNN_ROOT "${THIRD_PARTY_DIR}/${ONEDNN_DIR}")
+SET(THIRD_PARTY_DIR "${PROJECT_SOURCE_DIR}/third_party")
+SET(ONEDNN_DIR "oneDNN")
+SET(ONEDNN_ROOT "${THIRD_PARTY_DIR}/${ONEDNN_DIR}")
 
-  find_path(
-    ONEDNN_INCLUDE_DIR dnnl.hpp dnnl.h
-    PATHS ${ONEDNN_ROOT}
-    PATH_SUFFIXES include
-    NO_DEFAULT_PATH)
-  if(NOT ONEDNN_INCLUDE_DIR)
-    find_package(Git)
-    if(NOT Git_FOUND)
-      message(FATAL_ERROR "Can not find Git executable!")
-    endif()
-    execute_process(
+# find_library(ZE_LOADER_LIBRARY
+#     NAMES ze_loader
+#     PATHS "${CONDA_PREFIX}/Library/lib"
+#     NO_DEFAULT_PATH
+# )
+
+# message(STATUS "find ZE_LOADER_LIBRARY: ${ZE_LOADER_LIBRARY}")
+
+
+FIND_PATH(ONEDNN_INCLUDE_DIR dnnl.hpp dnnl.h PATHS ${ONEDNN_ROOT} PATH_SUFFIXES include NO_DEFAULT_PATH)
+IF(NOT ONEDNN_INCLUDE_DIR)
+    FIND_PACKAGE(Git)
+    IF(NOT Git_FOUND)
+      MESSAGE(FATAL_ERROR "Can not find Git executable!")
+    ENDIF()
+    EXECUTE_PROCESS(
       COMMAND ${GIT_EXECUTABLE} submodule update --init ${ONEDNN_DIR}
       WORKING_DIRECTORY ${THIRD_PARTY_DIR} COMMAND_ERROR_IS_FATAL ANY)
-    find_path(
-      ONEDNN_INCLUDE_DIR dnnl.hpp dnnl.h
-      PATHS ${ONEDNN_ROOT}
-      PATH_SUFFIXES include
-      NO_DEFAULT_PATH)
-  endif(NOT ONEDNN_INCLUDE_DIR)
+    FIND_PATH(ONEDNN_INCLUDE_DIR dnnl.hpp dnnl.h PATHS ${ONEDNN_ROOT} PATH_SUFFIXES include NO_DEFAULT_PATH)
+ENDIF(NOT ONEDNN_INCLUDE_DIR)
 
-  if(NOT ONEDNN_INCLUDE_DIR)
-    message(FATAL_ERROR "oneDNN source files not found!")
-  endif(NOT ONEDNN_INCLUDE_DIR)
+IF(NOT ONEDNN_INCLUDE_DIR)
+  MESSAGE(FATAL_ERROR "oneDNN source files not found!")
+ENDIF(NOT ONEDNN_INCLUDE_DIR)
 
-  set(DNNL_ENABLE_PRIMITIVE_CACHE
-      TRUE
-      CACHE BOOL "oneDNN sycl primitive cache" FORCE)
+SET(DNNL_ENABLE_PRIMITIVE_CACHE TRUE CACHE BOOL "oneDNN sycl primitive cache" FORCE)
 
-  set(DNNL_LIBRARY_TYPE
-      STATIC
-      CACHE STRING "" FORCE)
+SET(DNNL_LIBRARY_TYPE STATIC CACHE STRING "" FORCE)
 
-  set(DNNL_CPU_RUNTIME
-      "NONE"
-      CACHE STRING "oneDNN cpu backend" FORCE)
-  set(DNNL_GPU_RUNTIME
-      "SYCL"
-      CACHE STRING "oneDNN gpu backend" FORCE)
-  set(DNNL_BUILD_TESTS
-      FALSE
-      CACHE BOOL "build with oneDNN tests" FORCE)
-  set(DNNL_BUILD_EXAMPLES
-      FALSE
-      CACHE BOOL "build with oneDNN examples" FORCE)
-  set(DNNL_ENABLE_CONCURRENT_EXEC
-      TRUE
-      CACHE BOOL "multi-thread primitive execution" FORCE)
-  set(DNNL_EXPERIMENTAL
-      TRUE
-      CACHE BOOL "use one pass for oneDNN BatchNorm" FORCE)
+SET(DNNL_CPU_RUNTIME "THREADPOOL" CACHE STRING "oneDNN cpu backend" FORCE)
+SET(DNNL_GPU_RUNTIME "SYCL" CACHE STRING "oneDNN gpu backend" FORCE)
+SET(DNNL_BUILD_TESTS FALSE CACHE BOOL "build with oneDNN tests" FORCE)
+SET(DNNL_BUILD_EXAMPLES FALSE CACHE BOOL "build with oneDNN examples" FORCE)
+SET(DNNL_ENABLE_CONCURRENT_EXEC TRUE CACHE BOOL "multi-thread primitive execution" FORCE)
+SET(DNNL_EXPERIMENTAL TRUE CACHE BOOL "use one pass for oneDNN BatchNorm" FORCE)
 
-  add_subdirectory(${ONEDNN_ROOT} oneDNN EXCLUDE_FROM_ALL)
-  set(ONEDNN_LIBRARY ${DNNL_LIBRARY_NAME})
-  if(NOT TARGET ${ONEDNN_LIBRARY})
-    message(FATAL_ERROR "Failed to include oneDNN target")
-  endif(NOT TARGET ${ONEDNN_LIBRARY})
+ADD_SUBDIRECTORY(${ONEDNN_ROOT} oneDNN EXCLUDE_FROM_ALL)
+SET(ONEDNN_LIBRARY ${DNNL_LIBRARY_NAME})
+IF(NOT TARGET ${ONEDNN_LIBRARY})
+  MESSAGE(FATAL_ERROR "Failed to include oneDNN target")
+ENDIF(NOT TARGET ${ONEDNN_LIBRARY})
 
-  if(NOT APPLE AND CMAKE_COMPILER_IS_GNUCC)
-    target_compile_options(${ONEDNN_LIBRARY} PRIVATE -Wno-uninitialized)
-    target_compile_options(${ONEDNN_LIBRARY} PRIVATE -Wno-strict-overflow)
-    target_compile_options(${ONEDNN_LIBRARY} PRIVATE -Wno-error=strict-overflow)
-  endif(NOT APPLE AND CMAKE_COMPILER_IS_GNUCC)
+IF(NOT APPLE AND CMAKE_COMPILER_IS_GNUCC)
+  TARGET_COMPILE_OPTIONS(${ONEDNN_LIBRARY} PRIVATE -Wno-uninitialized)
+  TARGET_COMPILE_OPTIONS(${ONEDNN_LIBRARY} PRIVATE -Wno-strict-overflow)
+  TARGET_COMPILE_OPTIONS(${ONEDNN_LIBRARY} PRIVATE -Wno-error=strict-overflow)
+ENDIF(NOT APPLE AND CMAKE_COMPILER_IS_GNUCC)
 
-  target_compile_options(${ONEDNN_LIBRARY} PRIVATE -Wno-tautological-compare)
-  get_target_property(DNNL_INCLUDES ${ONEDNN_LIBRARY} INCLUDE_DIRECTORIES)
-  list(APPEND ONEDNN_INCLUDE_DIR ${DNNL_INCLUDES})
+TARGET_COMPILE_OPTIONS(${ONEDNN_LIBRARY} PRIVATE -Wno-tautological-compare)
+GET_TARGET_PROPERTY(DNNL_INCLUDES ${ONEDNN_LIBRARY} INCLUDE_DIRECTORIES)
+# TARGET_LINK_LIBRARIES(${ONEDNN_LIBRARY} PRIVATE ZE_LOADER)
+# target_link_libraries(${ONEDNN_LIBRARY} PRIVATE ${ZE_LOADER_LIBRARY})
+target_link_libraries(${ONEDNN_LIBRARY} 
+    PRIVATE "C:/Users/arda/miniforge3/envs/xin-vllm-xpu/Library/lib/ze_loader.lib"
+)
+list(APPEND ONEDNN_INCLUDE_DIR ${DNNL_INCLUDES})
 
-  # Upper level targets should not load header files from oneDNN's third party.
-  list(FILTER ONEDNN_INCLUDE_DIR EXCLUDE REGEX
-       ".*third_party/oneDNN/third_party.*")
+# Upper level targets should not load header files from oneDNN's third party.
+LIST(FILTER ONEDNN_INCLUDE_DIR EXCLUDE REGEX
+      ".*third_party/oneDNN/third_party.*")
 
-  set(ONEDNN_FOUND ON)
-  message(STATUS "Found oneDNN: TRUE")
+SET(ONEDNN_FOUND ON)
+MESSAGE(STATUS "Found oneDNN: TRUE")
 
-endif(NOT ONEDNN_FOUND)
+ENDIF(NOT ONEDNN_FOUND)
